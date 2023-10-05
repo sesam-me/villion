d6f64a0e71c1224241ed1b14f0801671b0a2bc9e
sein <lsi6601@gmail.com>
2023-10-05 11:20:03 +0900

    init

---
+++ src/main/java/com/project/villion/VillionApplication.java
@@ -0,0 +1,13 @@
+package com.project.villion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VillionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/villion/VillionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.villion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
